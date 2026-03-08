4255ded2a452add4aed5de1b941c9182c147d967
Otavio Yukio <yukiootavio@gmail.com>
2026-03-07 21:17:53 -0300

    feat: criar projeto

---
+++ src/main/java/io/github/otavioyukio/foodfly/FoodflyApplication.java
@@ -0,0 +1,13 @@
+package io.github.otavioyukio.foodfly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodflyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodflyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/otavioyukio/foodfly/FoodflyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.otavioyukio.foodfly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodflyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
