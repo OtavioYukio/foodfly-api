59e2acc30071860f3c6659d60f3b9565ea23f953
OtavioYukio <yukiootavio@gmail.com>
2026-03-08 12:35:57 -0300

    fix: corrigir ordem dos elementos generic

--- src/main/java/io/github/otavioyukio/foodfly/product/ProductRepository.java
+++ src/main/java/io/github/otavioyukio/foodfly/product/ProductRepository.java
@@ -2,5 +2,5 @@ package io.github.otavioyukio.foodfly.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Long, Product> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
