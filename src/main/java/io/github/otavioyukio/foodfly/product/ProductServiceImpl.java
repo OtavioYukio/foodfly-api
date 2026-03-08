a32a860ecdbd2302b19b7319f588a524a96160cf
OtavioYukio <yukiootavio@gmail.com>
2026-03-08 12:39:35 -0300

    feat: criar classe de implementação ProductServiceImpl e implementar o metodo listAllProducts

---
+++ src/main/java/io/github/otavioyukio/foodfly/product/ProductServiceImpl.java
@@ -0,0 +1,18 @@
+package io.github.otavioyukio.foodfly.product;
+
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> listAllProducts() {
+        return productRepository.findAll();
+    }
+}
